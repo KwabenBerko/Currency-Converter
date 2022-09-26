f56367f583b2c6fd0476300428a9c6a2658949bf
Kwabena Bio Berko <kwabenaberko95@gmail.com>
2022-09-26 01:50:20 +0000

    Minor Cleanup (#12)

--- sharedTest/src/commonMain/kotlin/com/kwabenaberko/sharedtest/testdouble/FakeGetSyncStatus.kt
+++ sharedTest/src/commonMain/kotlin/com/kwabenaberko/sharedtest/testdouble/FakeGetSyncStatus.kt
@@ -3,10 +3,9 @@ package com.kwabenaberko.sharedtest.testdouble
 import com.kwabenaberko.currencyconverter.domain.model.SyncStatus
 import com.kwabenaberko.currencyconverter.domain.usecase.GetSyncStatus
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
 
-class FakeGetSyncStatus: GetSyncStatus {
-    val result = MutableSharedFlow<SyncStatus>()
+class FakeGetSyncStatus : GetSyncStatus {
+    lateinit var result: Flow<SyncStatus>
 
     override fun invoke(): Flow<SyncStatus> {
         return result
