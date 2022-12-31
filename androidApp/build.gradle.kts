f529da598ed7be020543aea93d96c680aa450f14
Kwabena Berko <kwabenaberko95@gmail.com>
2022-12-31 21:31:56 +0000

    Update Android minSdk Version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     compileSdk = 33
     defaultConfig {
         applicationId = "com.kwabenaberko.currencyconverter.android"
-        minSdk = 22
+        minSdk = 24
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -49,20 +49,20 @@ dependencies {
     implementation(projects.converterTest)
     implementation("com.google.android.material:material:1.7.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha02")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha03")
     implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.3.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
     implementation("androidx.navigation:navigation-compose:2.5.3")
-    implementation("io.github.raamcosta.compose-destinations:animations-core:1.7.22-beta")
-    ksp("io.github.raamcosta.compose-destinations:ksp:1.7.22-beta")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
-    implementation("com.google.accompanist:accompanist-placeholder-material:0.27.0")
+    implementation("io.github.raamcosta.compose-destinations:animations-core:1.7.30-beta")
+    ksp("io.github.raamcosta.compose-destinations:ksp:1.7.30-beta")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
+    implementation("com.google.accompanist:accompanist-placeholder-material:0.28.0")
     implementation("com.airbnb.android:lottie-compose:5.2.0")
     implementation(libs.coroutines.core)
     implementation(libs.bundles.lifecycle)
     implementation(libs.kotlinx.collections)
-    debugImplementation("androidx.compose.ui:ui-tooling:1.3.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.3.2")
     testImplementation(projects.converterTest)
     testImplementation(libs.coroutines.test)
     testImplementation(libs.junit)
