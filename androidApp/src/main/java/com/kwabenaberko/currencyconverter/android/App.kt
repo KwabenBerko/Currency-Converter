f4d7b18e2052a79e73f8bc94aa98e7c0baecfb5d
Kwabena Bio Berko <kwabenaberko95@gmail.com>
2023-01-04 11:43:41 +0000

    Defer Sync UseCase Calls With WorkManager (#43)
    * Defer Sync UseCase Calls With WorkManager
    
    * Using Hacky OneTimeWorkRequestBuilder For Better Consistency
    
    * Remove Unused Imports

--- androidApp/src/main/java/com/kwabenaberko/currencyconverter/android/App.kt
+++ androidApp/src/main/java/com/kwabenaberko/currencyconverter/android/App.kt
@@ -1,14 +1,37 @@
 package com.kwabenaberko.currencyconverter.android
 
 import android.app.Application
+import android.util.Log
+import androidx.work.Configuration
+import androidx.work.DelegatingWorkerFactory
 import com.kwabenaberko.converter.factory.Container
 import com.kwabenaberko.converter.factory.ContainerFactory
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
-class App : Application() {
+class App : Application(), Configuration.Provider {
     lateinit var container: Container
+    private val mainScope = MainScope()
 
     override fun onCreate() {
         super.onCreate()
         container = ContainerFactory(context = this).makeContainer()
+        container.hasCompletedInitialSync()
+            .onEach { hasCompleted ->
+                if (hasCompleted) {
+                    SyncWorker.enqueue(this)
+                }
+            }.launchIn(mainScope)
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        val workerFactory = DelegatingWorkerFactory().apply {
+            addFactory(SyncWorker.Factory(container.sync))
+        }
+        return Configuration.Builder()
+            .setMinimumLoggingLevel(Log.DEBUG)
+            .setWorkerFactory(workerFactory)
+            .build()
     }
 }
---
+++ androidApp/src/main/java/com/kwabenaberko/currencyconverter/android/SyncWorker.kt
@@ -0,0 +1,61 @@
+package com.kwabenaberko.currencyconverter.android
+
+import android.content.Context
+import androidx.work.Constraints
+import androidx.work.CoroutineWorker
+import androidx.work.ExistingWorkPolicy
+import androidx.work.ListenableWorker
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
+import com.kwabenaberko.converter.domain.usecase.Sync
+import java.util.concurrent.TimeUnit
+
+class SyncWorker(
+    private val appContext: Context,
+    workerParameters: WorkerParameters,
+    private val sync: Sync
+) : CoroutineWorker(appContext, workerParameters) {
+
+    override suspend fun doWork(): Result {
+        return when (sync()) {
+            true -> {
+                enqueue(appContext, ExistingWorkPolicy.APPEND)
+                Result.success()
+            }
+            false -> Result.retry()
+        }
+    }
+
+    class Factory(private val sync: Sync) : WorkerFactory() {
+        override fun createWorker(
+            appContext: Context,
+            workerClassName: String,
+            workerParameters: WorkerParameters
+        ): ListenableWorker {
+            return SyncWorker(appContext, workerParameters, sync)
+        }
+    }
+
+    companion object {
+        private const val TAG = "sync"
+
+        fun enqueue(
+            context: Context,
+            existingWorkPolicy: ExistingWorkPolicy = ExistingWorkPolicy.KEEP
+        ) {
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
+
+            val syncWork = OneTimeWorkRequestBuilder<SyncWorker>()
+                .setInitialDelay(12, TimeUnit.HOURS)
+                .setConstraints(constraints)
+                .build()
+
+            WorkManager.getInstance(context).enqueueUniqueWork(TAG, existingWorkPolicy, syncWork)
+        }
+    }
+}
